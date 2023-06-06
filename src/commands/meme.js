edc7ae8f6ad2bedf01bfce48dd83879213debf4c
lucascossitt <lucascossitt@hotmail.com>
2023-06-06 19:51:06 -0300

    Possibilidade de busca no comando de meme

--- src/commands/meme.js
+++ src/commands/meme.js
@@ -6,13 +6,14 @@ module.exports = {
     showInHelp: true,
     run: async function (client, message, args) {
         try {
+            const busca = args[0] || 'darkmemers'
             await client.simulateTyping(message.chatId, true)
-            await reddit.GetRandompost('darkmemers')
+            await reddit.GetRandompost(busca)
                 .then(async response => {
                     if (response.ImageURL) {
                         await client.sendImage(message.chatId, response.ImageURL, 'meme.jpg')
-                        await client.simulateTyping(message.chatId, false)
                     }
+                    await client.simulateTyping(message.chatId, false)
                 })
                 .catch(err => console.error(err))
         } catch (err) {
