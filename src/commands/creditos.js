836069efd4d9fac481403d7acbf0dd66cf3e272c
lucascossitt <lucascossitt@inovaclick.com.br>
2023-06-15 23:35:29 -0300

    Comando de creditos

--- src/commands/creditos.js
+++ src/commands/creditos.js
@@ -4,7 +4,7 @@ module.exports = {
     showInHelp: true,
     run: async function (client, message, args) {
         try {
-            await client.sendText(message.chatId, `Siga meu criador no instagram: @cossitt_lucas`)
+            await client.sendText(message.chatId, `Siga meu criador no instagram: https://www.instagram.com/cossitt_lucas/`)
         } catch (err) {
             console.error(err)
         }
