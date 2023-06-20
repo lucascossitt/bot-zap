cbe32ea453435c342e010610753ca2de083f93af
lucascossitt <lucascossitt@inovaclick.com.br>
2023-06-20 15:38:37 -0300

    Sticker animado

--- src/commands/sticker.js
+++ src/commands/sticker.js
@@ -15,6 +15,15 @@ module.exports = {
                         await client.simulateTyping(message.chatId, false)
                     })
                     .catch(err => console.error(err))
+            } else if (message.type === 'video') {
+                await client.simulateTyping(message.chatId, true)
+                await zap
+                    .decryptMedia(message)
+                    .then(async mediaData => {
+                        await client.sendMp4AsSticker(message.chatId, mediaData)
+                        await client.simulateTyping(message.chatId, false)
+                    })
+                    .catch(err => console.error(err))
             }
         } catch (err) {
             console.error(err)
